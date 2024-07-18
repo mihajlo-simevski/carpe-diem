593c0980dae1fd3eba621b0d4188fe85e328c72f
Mihajlo Simevski <msimevskii@gmail.com>
2024-07-18 17:57:06 +0200

    feat: update product silce

--- src/store/productsSlice.ts
+++ src/store/productsSlice.ts
@@ -1,12 +1,24 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { Product } from "../mockdata";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Product, products } from "../mockdata";
+import { RootState } from "./store";
+
+export const fetchProducts = createAsyncThunk(
+  'products/fetchProducts',
+  async () => {
+    await new Promise(resolve => setTimeout(resolve, 500)); 
+    return products as unknown;
+  }
+);
 
 interface ProductsState {
   products: Product[];
+  status: 'idle' | 'loading' | 'succeeded' | 'failed';
 }
 
 const initialState: ProductsState = {
   products: [],
+  status: 'idle',
+  
 };
 
 export const productsSlice = createSlice({
@@ -17,8 +29,23 @@ export const productsSlice = createSlice({
       state.products = action.payload;
     },
   },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProducts.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.products = action.payload as Product[];
+      })
+      .addCase(fetchProducts.rejected, (state) => {
+        state.status = 'failed';
+      });
+  },
 });
 
 export const { setProducts } = productsSlice.actions;
 
 export default productsSlice.reducer;
+
+export const selectProducts = (state: RootState) => state.products.products;
