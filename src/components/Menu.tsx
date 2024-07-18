f32db5fdd4866d35528c7b5e008ba8239fb9e1cb
Mihajlo Simevski <msimevskii@gmail.com>
2024-07-18 14:45:22 +0200

    feat: fetching and displaying data

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,12 +1,21 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Category } from "../mockdata";
+import { fetchCategories, selectCategories } from "../store/categoriesSlice";
 import MenuItem from "./MenuItem";
-import { categories, Category } from "../mockdata";
 
 export default function Menu() {
+  const dispatch = useDispatch();
+  const categories = useSelector(selectCategories);
+
+  useEffect(() => {
+    dispatch(fetchCategories() as any);
+  }, [dispatch]);
   return (
     <div className="mt-6">
       <h3 className="text-lg font-bold">Menu</h3>
       <div className="grid grid-cols-2 gap-4 mt-4 md:grid-cols-3 lg:grid-cols-5">
-      {categories.map((category: Category) => (
+        {categories.map((category: Category) => (
           <MenuItem
             key={category.id}
             id={category.id}
--- src/store/categoriesSlice.ts
+++ src/store/categoriesSlice.ts
@@ -1,12 +1,23 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { Category } from '../mockdata';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { categories, Category } from '../mockdata';
+import { RootState } from './store';
+
+export const fetchCategories = createAsyncThunk(
+  'categories/fetchCategories',
+  async () => {
+    await new Promise(resolve => setTimeout(resolve, 500));
+    return categories as unknown;
+  }
+);
 
 interface CategoriesState {
   categories: Category[];
+  status: 'idle' | 'loading' | 'succeeded' | 'failed';
 }
 
 const initialState: CategoriesState = {
   categories: [],
+  status: 'idle',
 };
 
 export const categoriesSlice = createSlice({
@@ -15,11 +26,25 @@ export const categoriesSlice = createSlice({
   reducers: {
     setCategories: (state, action: PayloadAction<Category[]>) => {
       state.categories = action.payload;
-    },
-    
+    }, 
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCategories.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchCategories.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.categories = action.payload as Category[];
+      })
+      .addCase(fetchCategories.rejected, (state) => {
+        state.status = 'failed';
+      });
   },
 });
 
 export const { setCategories } = categoriesSlice.actions;
 
 export default categoriesSlice.reducer;
+
+export const selectCategories = (state: RootState) => state.categories.categories;
